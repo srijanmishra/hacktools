658e7feb5a05f78c0a88468b23cb4f086f74e5c3
Rohit Yadav <rohityadav89@gmail.com>
2011-02-09 06:31:20 +0530

    s3uploader: handles mimetypes better, uploads only one file

--- s3uploader/s3uploader.py
+++ s3uploader/s3uploader.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python
 import sys
 import mimetypes
 
@@ -8,34 +8,37 @@ from boto.s3.connection import VHostCallingFormat
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
-#Put your keys in awskeys.py: ACCESS_KEY, PASS_KEY
-#chmod to 400 and put 'em in .gitignore :)
 from awskeys import ACCESS_KEY, PASS_KEY
 
-def uploadFile(bucket, filename):
+def uploadFile(bucket, filename, mime=""):
     conn = S3Connection(ACCESS_KEY, PASS_KEY)
     b = conn.create_bucket(bucket)
-    mime = mimetypes.guess_type(filename)[0]
+    if mime == "":
+        mime = mimetypes.guess_type(filename)[0]
+        if mime == "":
+            print "No default mime set, please use -m in args"
+            return
+    print "Uploading file %s of mimetype %s" % (filename, mime)
     k = Key(b)
     k.key = filename
     k.set_metadata("Content-Type", mime)
     k.set_contents_from_filename(filename)
     k.set_acl("public-read")
 
-
 def main():
     parser = OptionParser()
     parser.add_option("-b", "--bucket", dest="bucket", default="hotbuffer", help="s3 bucket name!")
     parser.add_option("-f", "--file", dest="filename", default="", help="filename")
-    parser.add_option("-d", "--debug", dest="debug", action="store_true", default=False, help="enable debugging")
+    parser.add_option("-m", "--mime", dest="mime", default="", help="set mimetype")
     (options, args) = parser.parse_args()
 
     bucket = options.bucket
-    debug = options.debug
+    mime = options.mime
     filename = options.filename
 
+    print "Uploading in bucket:", bucket
     if filename != "":
-        uploadFile(bucket, filename)
+        uploadFile(bucket, filename, mime)
     else:
         print "Yo dawg, you forget to put some file in ya bucket man..."
 
