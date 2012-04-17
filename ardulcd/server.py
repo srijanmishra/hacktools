a13818f5ed53c3b16d83e2e72a432b8be7f34b7f
Rohit Yadav <rohityadav89@gmail.com>
2012-04-17 21:37:32 +0530

    ardulcd: show sound data by taking it's FFT and ploting on the LCD as bar graph

--- ardulcd/server.py
+++ ardulcd/server.py
@@ -3,9 +3,33 @@
 import serial
 import os, sys, time
 
+import pyaudio
+from scipy import fft
+from numpy import short, fromstring
+import math
+
 from execproc import Monitor
 
+s = serial.Serial()
+
+stream = None
+samples = 16
+s_rate = 11000
+def getFFT():
+  global stream
+  if stream is None:
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=s_rate, input=True)
+
+  audiodata = fromstring(stream.read(samples), dtype=short)
+  normalized_data = audiodata / 32768.0
+  fft_data = fft(normalized_data)
+  fft_max  = max(abs(fft_data))
+  norm_fft = map(lambda x: int(math.ceil(x)), abs(fft_data) * 7 / fft_max)
+  return norm_fft
+
 def getDevice():
+  global s
   paths = []
   for num in range(5):
     path = '/dev/ttyUSB%d' % num
@@ -29,7 +53,17 @@ def getDevice():
 def writenum(s, n):
   s.write(chr(n))
 
-def cls(s):
+def customChars():
+  global s
+  for i in range(248, 256):
+    writenum(s, i)
+    p = 248 + 8 - i
+    for c in range(p-1):
+      writenum(s, 0)
+    for c in range(8-p+1):
+        writenum(s, 31)
+
+def cls():
   """
   Clears LCD
   22: curoff, noblink
@@ -37,33 +71,25 @@ def cls(s):
   24: curon, noblink
   25: curon, blink
   """
+  global s
   writenum(s, 24)
 
-def backlight(s, p):
+def backlight(p):
+  global s
   if p:
     writenum(s, 17)
   else:
     writenum(s, 18)
 
-def goto(s, line, column):
+def goto(line, column):
+  global s
   if line == 0:
     s.write(128+column)
   else:
     s.write(148+column)
 
-dev = getDevice()
-if dev == "":
-  sys.exit()
-s = serial.Serial(dev, 9600)
-print "Serial connection established with %s at 9600 baud" % dev
-s.timeout = 2
-s.readall()
-
-cls(s)
-cls(s)
-cls(s)
-
 def pl(st):
+  global s
   st = st[:16]
   line = [' '] * 16
   for i in range(len(st)):
@@ -71,14 +97,28 @@ def pl(st):
   s.write(''.join(line))
   s.write('\r')
 
-pl("Rohit Yadav")
-pl("antrix.yaan.in")
-shell = Monitor('rohit')
-while True:
-  mem = float(shell.run("ps aux | grep -v MEM | awk '{sum += $4} END {print sum}'"))
-  cpu = float(shell.run("ps aux | grep -v CPU | awk '{sum += $3} END {print sum/8}'"))
-  temp = float(shell.run("""sensors | grep 'Core.*+' | awk '{print $3}' | sed 's/+//' | sed 's/.C//' | awk '{sum+=$0} END {print sum/4}'"""))
-  print mem, cpu, temp
-  pl("CPU:%0.2f @ %0.1fC" % (cpu, temp))
-  pl("Mem:%0.2fGB" % (mem*4/100))
-  time.sleep(2)
+def repl():
+  global s
+  shell = Monitor('rohit')
+  while True:
+    mem = float(shell.run("ps aux | grep -v MEM | awk '{sum += $4} END {print sum}'"))
+    cpu = float(shell.run("ps aux | grep -v CPU | awk '{sum += $3} END {print sum/8}'"))
+    temp = float(shell.run("""sensors | grep 'Core.*+' | awk '{print $3}' | sed 's/+//' | sed 's/.C//' | awk '{sum+=$0} END {print sum/4}'"""))
+    pl("%0.1f%% %0.1fG %0.1fC" % (cpu, mem*4/100,temp))
+    sound_data = getFFT()
+    for i in range(16):
+      writenum(s, sound_data[i])
+    s.write('\r')
+    time.sleep(0.2)
+
+dev = getDevice()
+if dev == "":
+  sys.exit()
+s = serial.Serial(dev, 9600)
+s.timeout = 1
+s.readall()
+
+customChars()
+cls()
+repl()
+
