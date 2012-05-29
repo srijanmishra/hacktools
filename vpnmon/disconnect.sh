3ec120bb9809b4036018183584edee66bc3c9548
Rohit Yadav <rohityadav89@gmail.com>
2012-05-30 01:47:18 +0530

    updated to latest version

--- vpnmon/disconnect.sh
+++ vpnmon/disconnect.sh
@@ -2,5 +2,5 @@
  
 status="DISCONNECT|`date`|$common_name|$trusted_ip:$trusted_port|$ifconfig_pool_local_ip:$ifconfig_pool_remote_ip|$bytes_received|$bytes_sent"
 echo $status >> /var/log/openvpn/disconnect.log
-/etc/openvpn/updateuserdata.py $common_name $bytes_received $bytes_sent
+/etc/openvpn/updateuserdata.py $common_name $bytes_received $bytes_sent 
 exit 0;
